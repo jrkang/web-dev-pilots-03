62b4d4d81a0a7c4574b4766010f68ebf7d3bf592
jrkang <jaerkang@gmail.com>
2014-09-05 14:12:26 +0900

    09/05까지

--- WebContent/js/app/routers/baseRouter.js
+++ WebContent/js/app/routers/baseRouter.js
@@ -3,45 +3,66 @@ define(function(require) {
 	"use strict";
 
 	// require library
-	var $ = require('jquery'), Backbone = require('backbone');
+	var $ = require('jquery'),
+		Backbone = require('backbone');
 
 	// require View
-	var frameView;
-	
-	var $container;
+	var frameView, $container;
+
+	var currentView;
 
 	return Backbone.Router.extend({
 
-		routes : {
-			"" : "login",
-			"login" : "login",
-			"dashboard" : "dashboard",
-			"sessionExpire" : "sessionExpire"
+		routes: {
+			"": "login",
+			"login": "login",
+			"dashboard": "dashboard",
+			"realtime": "realtime",
+			"sessionExpire": "sessionExpire"
 		},
-		setOptions : function(options) {
+		setOptions: function(options) {
 			$container = options.$container;
-			
+
 			frameView = options.frameView;
 		},
-		login : function() {
-			$container.empty();
-			$container.removeClass();
-			var LoginView = require('views/login');
-			var loginView = new LoginView({
-				el : $container
-			}).render();
-		},
-		dashboard : function() {
-			require([ 'views/dashboard' ], function(Dashboard) {
-				var dashboard = new Dashboard({
-					el : $container
+		login: function() {
+			$.when(this.closeCurrentView()).done(function() {
+				var LoginView = require('views/login');
+				currentView = new LoginView({
+					el: $container
 				}).render();
 			});
-			
-			frameView.selectMenuItem('');
 		},
-		sessionExpire : function() {
+		dashboard: function() {
+			$.when(this.closeCurrentView()).done(function() {
+				require(['views/dashboard'], function(Dashboard) {
+					currentView = new Dashboard({
+						el: $container
+					}).render();
+				});
+
+				frameView.selectMenuItem('');
+			});
+		},
+		realtime: function() {
+			$.when(this.closeCurrentView()).done(function() {
+				require(['views/realtime/usingSockjs'], function(UsingSockjs) {
+					currentView = new UsingSockjs({
+						el: $container
+					}).render();
+				});
+
+				frameView.selectMenuItem('');
+			});
+		},
+		sessionExpire: function() {
 			$('body').html('session expired.');
+		},
+		closeCurrentView: function() {
+			if (currentView !== undefined) {
+				currentView.close();
+				currentView = undefined;
+			}
 		}
 
 	});
